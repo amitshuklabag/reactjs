592f0bb61b868aba99c2083645b223a57e41a894
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-03-21 16:46:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Elestio CI/CD Deployment
+          Learn React with Elestio CI/CD Deployment testing
         </a>
       </header>
     </div>
